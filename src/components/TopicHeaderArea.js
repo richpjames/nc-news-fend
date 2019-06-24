d02aa006c08e2bfd07a8bbae358f1a2ba91f14ba
ricojamjar <rich@genericgreeting.co.uk>
2019-06-24 15:21:07 +0100

    Adding topic links

---
+++ src/components/TopicHeaderArea.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TopicHeaderArea(props) {
+  return (
+    <div>
+      <ul style={{ float: "left" }}>
+        { props.topics.map(topic => (
+          <li> {topic}</li>
+        ))
+        }
+      </ul>
+    </div>
+  );
+}
