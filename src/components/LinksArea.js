590ef9c94358faa6be9f11bd60aa6f6087666931
ricojamjar <rich@genericgreeting.co.uk>
2019-06-24 15:20:22 +0100

    Adding links

---
+++ src/components/LinksArea.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LinksArea() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Articles</li>
+      </ul>
+    </div>
+  );
+}
