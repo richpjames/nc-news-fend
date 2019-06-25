4001ee71a76d07e3fff4a699fb7735f8af6c5b5e
ricojamjar <rich@genericgreeting.co.uk>
2019-06-25 10:13:25 +0100

    changing index to reflect linting rules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "normalize.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
