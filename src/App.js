a0c5c363442c86f0d2d8dd3d196aed6e73c6fffa
ricojamjar <rich@genericgreeting.co.uk>
2019-06-25 15:50:48 +0100

    adding fetch articles functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
 import "./App.css";
-import { getTopics } from "./Api";
-import styled, { css } from "styled-components";
+import { getTopics, getArticleSummaries } from "./Api";
 import ArticlesGrid from "./components/ArticlesGrid";
+import Header from "./components/Header";
+import TopicPage from "./components/TopicPage";
+import { Router } from "@reach/router";
 
 class App extends Component {
   state = {
-    topics: []
+    topics: [],
+    articles: []
   };
 
   render() {
-    const { topics } = this.state;
+    const { topics, articles } = this.state;
     return (
       <div className="App">
         <Header topics={topics} />
-        <ArticlesGrid />
+        <Router>
+          <ArticlesGrid path="/" />
+          <TopicPage path="/topics/:topicSlug" articles={articles} />
+        </Router>
       </div>
     );
   }
   componentDidMount() {
     getTopics().then(topics => this.setState({ topics: topics }));
+    this.fetchArticles();
   }
+  fetchArticles = () => {
+    const { topic } = this.props;
+
+    getArticleSummaries(topic)
+      .then(articles =>
+        this.setState({ articles, loading: false, hasError: false })
+      )
+      .catch(error => this.setState({ hasError: error, loading: false }));
+  };
 }
 
 export default App;
