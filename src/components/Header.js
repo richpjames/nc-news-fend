5f861f802a63195f1eac840857f9f26e1fe21514
ricojamjar <rich@genericgreeting.co.uk>
2019-06-25 10:09:49 +0100

    adding styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,20 @@ import React from "react";
 import LinksArea from "./LinksArea";
 import TopicHeaderArea from "./TopicHeaderArea";
 import LoggedInArea from "./LoggedInArea";
+import styled, { css } from "styled-components";
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-around;
+`;
 
 export default function Header(props) {
   return (
-    <div>
+    <HeaderWrapper>
       <TopicHeaderArea topics={props.topics} />
       <h1>NC News</h1>
       <LoggedInArea />
       <LinksArea />
-    </div>
+    </HeaderWrapper>
   );
 }
