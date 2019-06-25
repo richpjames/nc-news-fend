1c7b0172b153811d27af2c312f639ca1cd850549
ricojamjar <rich@genericgreeting.co.uk>
2019-06-25 18:59:30 +0100

    changing LinksArea to ViewAllArticlesLink

--- src/components/LinksArea.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LinksArea() {
-  return (
-    <div>
-      <h4>View All Articles</h4>
-    </div>
-  );
-}
---
+++ src/components/ViewAllArticlesLink.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+export default function LinksArea() {
+  return (
+    <Link to="/">
+      <div>
+        <h4>View All Articles</h4>
+      </div>
+    </Link>
+  );
+}
