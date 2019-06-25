bf95f1e0970d7eedfaaca2323628a34c52dde4f7
ricojamjar <rich@genericgreeting.co.uk>
2019-06-25 10:12:02 +0100

    using get request to set state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import "./App.css";
+import { getTopics } from "./Api";
+import styled, { css } from "styled-components";
+import ArticlesGrid from "./components/ArticlesGrid";
 
 class App extends Component {
   state = {
-    topics: ["topic1", "topic2", "topic3"]
+    topics: []
   };
+
   render() {
     const { topics } = this.state;
     return (
       <div className="App">
         <Header topics={topics} />
+        <ArticlesGrid />
       </div>
     );
+  }
+  componentDidMount() {
+    getTopics().then(topics => this.setState({ topics: topics }));
   }
 }
 
