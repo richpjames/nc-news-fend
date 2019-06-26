546644ee0728da2acaf88bf969523ded472aac0f
ricojamjar <rich@genericgreeting.co.uk>
2019-06-26 20:56:42 +0100

    Adding key to comments display

--- src/components/comments/CommentDisplay.js
+++ src/components/comments/CommentDisplay.js
@@ -8,12 +8,12 @@ const CommentDisplay = props => {
     <>
       <h2>Comments:</h2>
       {props.comments.map(comment => (
-        <>
+        <div key={comment.created_at}>
           <p>{comment.body}</p>
           <h4>{comment.author}</h4>
           <p>{comment.votes}</p>
           <p>{comment.created_at}</p>
-        </>
+        </div>
       ))}
     </>
   );
