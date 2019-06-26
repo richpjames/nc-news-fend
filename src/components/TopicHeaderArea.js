7f3c626d38921d3da8f324eacb19c9feac997d6b
ricojamjar <rich@genericgreeting.co.uk>
2019-06-26 15:52:12 +0100

    adding topic wrapper

--- src/components/TopicHeaderArea.js
+++ src/components/TopicHeaderArea.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled from "styled-components";
 
 export default function TopicHeaderArea(props) {
+  const TopicLinkWrapper = styled.span`
+    border: 1px solid #6fb1fc;
+    padding: 0.5em;
+  `;
   return (
     <div>
       <ul>
-        {props.topics.map(topic => (
-          <Link to={`/topics/${topic.slug}`} key={topic.slug}>
-            <li>{topic.slug}</li>
-          </Link>
+        {props.topics.map((topic, i) => (
+          <TopicLinkWrapper key={topic.slug}>
+            <Link to={`/topics/${topic.slug}`}>
+              <li>{topic.slug}</li>
+            </Link>
+          </TopicLinkWrapper>
         ))}
       </ul>
     </div>
