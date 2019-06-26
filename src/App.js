079f527233a6707244b955e8ce7fb2afb7fb1351
ricojamjar <rich@genericgreeting.co.uk>
2019-06-26 15:52:58 +0100

    adding functionality get article by id

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
-import { getTopics, getArticleSummaries } from "./Api";
 import ArticlesGrid from "./components/ArticlesGrid";
+import SingleArticle from "./components/SingleArticle";
 import Header from "./components/Header";
-import TopicPage from "./components/TopicPage";
 import { Router } from "@reach/router";
-import styled, { css } from "styled-components";
+import { getTopics } from "./Api.js";
 
 class App extends Component {
   state = {
-    topics: [],
-    articles: [],
-    hasError: false,
-    loading: true,
-    topic: ""
+    topics: []
   };
   render() {
-    const { topics, articles } = this.state;
+    const { topics } = this.state;
     return (
       <div className="App">
         <Header topics={topics} />
         <Router>
           <ArticlesGrid path="/" />
-          <ArticlesGrid path="/topics/:topicSlug" />
+          <ArticlesGrid path="/topics/:topic" />
+          <SingleArticle path="/article/:article_id" />
         </Router>
       </div>
     );
   }
   componentDidMount() {
     getTopics().then(topics => this.setState({ topics: topics }));
-    this.fetchArticles();
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.topic !== this.props.topic) {
-      this.fetchArticles();
-    }
-  }
-
-  fetchArticles = () => {
-    const { topic } = this.props;
-    getArticleSummaries(topic)
-      .then(articles =>
-        this.setState({ articles, loading: false, hasError: false })
-      )
-      .catch(error => this.setState({ hasError: error, loading: false }));
-  };
 }
 
 export default App;
