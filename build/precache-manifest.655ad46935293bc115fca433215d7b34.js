872afc0e0ebc6a0f6cd5e5dd00ab9edeee30913c
ricojamjar <rich@genericgreeting.co.uk>
2019-07-01 09:28:52 +0100

    readying netlify deployment

---
+++ build/precache-manifest.655ad46935293bc115fca433215d7b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f47eca051f03b8cad9f623373996687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88dff35b18156875be46",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4ef6dd14b61887bdc917",
+    "url": "/static/css/main.b568c09f.chunk.css"
+  },
+  {
+    "revision": "88dff35b18156875be46",
+    "url": "/static/js/2.bba81256.chunk.js"
+  },
+  {
+    "revision": "4ef6dd14b61887bdc917",
+    "url": "/static/js/main.14195fcf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
