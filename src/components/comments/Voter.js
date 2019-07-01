60e9a65b278bfc884e43dbddbd43aef891ba4727
ricojamjar <rich@genericgreeting.co.uk>
2019-07-01 15:27:34 +0100

    styling vote buttons

--- src/components/comments/Voter.js
+++ src/components/comments/Voter.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import { patchArticleVotes, patchCommentVotes } from "../../Api";
+import styled from "styled-components";
+
+const Thumb = styled.button`
+  height: 25px;
+  position: relative;
+`;
 
 export default class Voter extends Component {
   state = {
     voteChange: 0
   };
-
   render() {
     const { voteChange } = this.state;
     const { votes } = this.props;
     return (
-      <div>
-        <button onClick={() => this.handleVote(1)} disabled={voteChange > 0}>
+      <section>
+        <Thumb onClick={() => this.handleVote(1)} disabled={voteChange > 0}>
           <i className="fas fa-thumbs-up" />
-        </button>
+        </Thumb>
+        <br />
         Votes: {votes + voteChange}
-        <button onClick={() => this.handleVote(-1)} disabled={voteChange < 0}>
+        <br />
+        <Thumb onClick={() => this.handleVote(-1)} disabled={voteChange < 0}>
           <i className="fas fa-thumbs-down" />
-        </button>
-      </div>
+        </Thumb>
+      </section>
     );
   }
   handleVote = increment => {
