4dcc9b1d08986bf8bde37dd52a7e691e53f38b7d
ricojamjar <rich@genericgreeting.co.uk>
2019-07-07 09:05:19 +0100

    Improving clarity of logged in info

--- src/components/header/LoggedInArea.js
+++ src/components/header/LoggedInArea.js
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 const LoggedInWrapper = styled.div`
-  height: 2px;
-  display: inline-block;
+  height: 20px;
+  float: right;
 `;
 
 export default function LoggedInArea() {
   return (
     <LoggedInWrapper>
-      <p>You are logged in as: a user</p>
+      <p>You are logged in as: jessjelly</p>
     </LoggedInWrapper>
   );
 }
